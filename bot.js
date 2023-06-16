5983e25af25f97fdd38e6b77948383759b40e4c4
Nazaryeva <133712306+Nazaryeva@users.noreply.github.com>
2023-06-16 19:54:47 +0300

    Добавила работу на целевом сайте и в выдаче
    Умеет ходить не дальше 4 страницы

--- bot.js
+++ bot.js
@@ -1,33 +1,70 @@
 // ==UserScript==
 // @name         BingBot
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  try to take over the world!
 // @author       Nazareva Anna
 // @match        https://www.bing.com/*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @grant        none
 // ==/UserScript==
- 
-let keywords = ["10 самых популярных шрифтов от Google", "Отключение редакций и ревизий в WordPress", "Вывод произвольных типов записей и полей в WordPress"];
+
+let keywords = ["Как использовать devtools браузера", "10 самых популярных шрифтов от Google", "Отключение редакций и ревизий в WordPress"];
 let keyword = keywords[getRandom(0, keywords.length)];
 let links = document.links;
 let btnK = document.getElementById("search_icon");
- 
+let bingInput = document.getElementsByName("q")[0];
+
 if(btnK != null) {
-    document.getElementsByName("q")[0].value = keyword;
-    btnK.click();
+    let i = 0;
+    let timerId = setInterval(() => {
+        bingInput.value += keyword[i];
+        i++;
+        if(i == keyword.length) {
+            clearInterval(timerId);
+            btnK.click();
+        }
+    }, 500);
+} else if(location.hostname == "napli.ru") {
+    console.log("мы на целевом сайте");
+    setInterval(() => {
+        let index = getRandom(0, links.length);
+        if(getRandom(0, 101) >= 70) {
+            location.href = "https://www.bing.com/";
+        }
+        if(links[index].href.indexOf("napli.ru") != -1) links[index].click()
+    }, getRandom(2000, 5000))
 } else {
+    let nextBingPage = true;
     for(let i = 0; i < links.length; i++) {
         if (links[i].href.indexOf("napli.ru") != -1) {
             let link = links[i];
+            nextBingPage = false;
             console.log("нашел " + link);
-            link.click();
+            setTimeout(() => {
+                link.click();
+            }, getRandom(2000, 5000))
             break;
         }
     }
+    let elementExist = setInterval(() => {
+        let element = document.getElementsByClassName("sb_pagS sb_pagS_bp b_widePag sb_bp")[0];
+        if (element != null) {
+            if(element.innerText == "4") {
+                nextBingPage = false;
+                location.href = "https://www.bing.com/";
+            }
+            clearInterval(elementExist);
+        }
+    }, 100)
+
+    if(nextBingPage) {
+        setTimeout(() => {
+            document.getElementsByClassName("sw_next")[0].click();
+        }, getRandom(3000, 8000))
+    }
 }
- 
+
 function getRandom(min, max) {
     return Math.floor(Math.random() * (max - min) + min)
 }
